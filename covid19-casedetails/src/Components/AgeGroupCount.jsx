64e2120b204bf80121e032670de6ab3bd40a772e
Aloy <aloyleow@gmail.com>
2024-08-11 01:40:46 +0800

    fix: updated  recharts library to alpha version

--- covid19-casedetails/src/Components/AgeGroupCount.jsx
+++ covid19-casedetails/src/Components/AgeGroupCount.jsx
@@ -1,26 +1,34 @@
+
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts"
 import { useEffect, useState } from "react"
 
 
-export default function ImportedLocalCount({covidData}){
-
+export default function AgeGroupCount({covidData}){
+    
+    const [ageToNum, setAgeToNum] = useState()
     const [importedLocal, setImportedLocal] = useState()
     
+
     useEffect(() => {
-        let x = 0
-        let y = 0
-        for(let i = 0; i < covidData.length; i ++){
-            if (covidData[i].imported_local === "Imported"){
-                x += 1
-            } else if (covidData[i].imported_local === "Local"){
-                y += 1
-            }
-        }
-        setImportedLocal([{name: "Imported", value: x}, {name: "Local", value: y}])
-        console.log(x, y)
+       covidData.map((age) => {setAgeToNum([...ageToNum, age.age])})
+       
+    }, [])
+    
+    console.log(ageToNum)
+    // useEffect(() => {
+    //     let x = 0
+    //     let y = 0
+    //     for(let i = 0; i < covidData.length; i ++){
+    //         if (covidData[i].imported_local === "Imported"){
+    //             x += 1
+    //         } else if (covidData[i].imported_local === "Local"){
+    //             y += 1
+    //         }
+    //     }
+    //     setImportedLocal([{name: "Imported", value: x}, {name: "Local", value: y}])
+    //     console.log(x, y)
 
-    }, [covidData])
-
+    // }, [covidData])
     
     
     return (<>
--- covid19-casedetails/src/Pages/DataAnalysisPage.jsx
+++ covid19-casedetails/src/Pages/DataAnalysisPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import GenderCount from "../Components/GenderCount"
 import ImportedLocalCount from "../Components/ImportedLocalCount"
+import AgeGroupCount from "../Components/AgeGroupCount"
 
  
 export default function DataAnalysisPage({covidData}){
@@ -20,9 +21,11 @@ export default function DataAnalysisPage({covidData}){
             <select value = {analysisSelected} onChange= {(event)=>handleChange(event)}>
                 <option>Gender Count</option>
                 <option>Imported vs Local</option>
+                <option>Age Group</option>
             </select>
             {analysisSelected === "Gender Count"?<GenderCount covidData = {covidData}/>:"" }
             {analysisSelected === "Imported vs Local"?<ImportedLocalCount covidData = {covidData} />:""}
+            {analysisSelected === "Age Group"?<AgeGroupCount covidData = {covidData}/>:"" }
         </div>
     
     </>)
